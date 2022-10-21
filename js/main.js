6eb0847c1da964a0277bfd59881426cc3e9350be
o2ber <Oscar.De-Berranger@etu.univ-grenoble-alpes.fr>
2022-10-21 15:16:11 +0200

    yg

--- js/main.js
+++ js/main.js
@@ -1,16 +1,55 @@
 const cells = document.querySelectorAll('.cell')
-const chris = document.querySelector('.chrisRock')
 const score = document.querySelector('#score')
 const timeLeft = document.querySelector('#timeLeft')
+const starter = document.querySelector('.starter')
+const stop = document.querySelector('.stop')
 
-let position;
+let position
+let timer = null
+let currentScore = 0
+
+starter.addEventListener('click', ()=>{
+    timer = 30
+})
+stop.addEventListener('click', ()=>{
+    timer = 0
+    timeLeft.innerHTML = timer
+    score.innerHTML = 0
+})
+
+cells.forEach(cell=>{
+    cell.addEventListener('mousedown', ()=>{
+        if (cell.id === position){
+            currentScore++
+            score.innerHTML = currentScore
+            position = null
+            cell.classList.remove('chrisRock')
+        }
+    })
+})
 
 function randomlySpawnChris(){
+    if (!timer) return
 
+    cells.forEach(cell=>{cell.classList.remove('chrisRock')})
+
+    let randomCell = cells[Math.floor(Math.random()*9)]
+    randomCell.classList.add('chrisRock')
+
+    position = randomCell.id //id de la cellule qui affiche chris
 }
 
-// definir un chiffre au hasard entre 0 et 8 (inclus)
-//Math.floor(Math.random()*9)
-// cells[1].classList.add('chrisRock')
 
+function countdown(){
+    if (!timer) return
+    timer--
+    timeLeft.innerHTML = `${timer} seconds`
+    if (timer === 0){
+        alert(`Game Over your score is : ${currentScore}`)
+        currentScore = 0
+    }
+}
+countdown()
 
+let mooveChris = setInterval(randomlySpawnChris, 700)
+let startCoundown = setInterval(countdown, 1000)
